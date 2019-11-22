275d23c763d555596198afe28d6a7c9ddeeb63f3
KHong <albtrossfacefall@gmail.com>
2019-11-22 09:52:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array_strings, event_name) {
+  let event_array = [];
+  for (let i=0; i<array_strings.length; i++) {
+    event_array[i] = `Thank you, ${array_strings[i]}, for the wonderful ${event_name} gift!`;
+  }
+  return event_array;
+}
+
+function countDown(integer) {
+  while (integer>=0) {
+    console.log(integer);
+    integer --;
+  }
+}
